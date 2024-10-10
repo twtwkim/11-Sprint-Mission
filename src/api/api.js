81173120f61eabb33f572b87c8c63c61486ca9c4
twtwkim <alfog23@gmail.com>
2024-10-10 21:35:50 +0900

    Use try-catch and URLSearchParams in api.js

--- src/api/api.js
+++ src/api/api.js
@@ -4,13 +4,18 @@ export async function getProducts({
   orderBy = "",
   keyword = "",
 }) {
-  const query = `page=${page}&pageSize=${pageSize}&orderBy=${orderBy}&keyword=${keyword}`;
-  const response = await fetch(
-    `https://panda-market-api.vercel.app/products?${query}`
-  );
-  if (!response.ok) {
+  const params = new URLSearchParams({ page, pageSize, orderBy, keyword });
+
+  try {
+    const response = await fetch(
+      `https://panda-market-api.vercel.app/products?${params}`
+    );
+    if (!response.ok) {
+      throw new Error("서버에서 오류 응답을 받았습니다.");
+    }
+    const body = await response.json();
+    return body;
+  } catch (error) {
     throw new Error("데이터를 불러오는데 실패했습니다.");
   }
-  const body = await response.json();
-  return body;
 }
